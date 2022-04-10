7613a00044ec8d168c1367d0d8cd886b5c68d582
morsa <voxfoot@gmail.com>
2022-04-11 00:13:06 +0500

    test: implement some tests

--- __tests__/genDiffFlat.test.js
+++ __tests__/genDiffFlat.test.js
@@ -1,27 +1,30 @@
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
 import genDiffFlat from '../bin/genDiffFlat.js';
 
-test('difference object', () => {
-  const obj1 = {
-    host: 'hexlet.io',
-    timeout: 50,
-    proxy: '123.234.53.22',
-    follow: false,
-  };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-  const obj2 = {
-    timeout: 20,
-    verbose: true,
-    host: 'hexlet.io',
-  };
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-  const result = genDiffFlat(obj1, obj2);
+describe('Flat JSON files', () => {
+  const obj1 = JSON.parse(readFileSync(getFixturePath('file1.json')));
+  const obj2 = JSON.parse(readFileSync(getFixturePath('file2.json')));
+  const flatObj = genDiffFlat(obj1, obj2);
 
-  expect(result).toStrictEqual({
-    '- follow': false,
-    '  host': 'hexlet.io',
-    '- proxy': '123.234.53.22',
-    '- timeout': 50,
-    '+ timeout': 20,
-    '+ verbose': true,
+  test('Length and sort', () => {
+    const keys = Object.keys(flatObj);
+    expect(keys).toHaveLength(6);
+    expect(keys[0]).toBe('- follow');
+  });
+
+  test('value', () => {
+    expect(flatObj['- follow']).toBe(false);
+    expect(flatObj['+ timeout']).toBe(20);
+  });
+
+  test('match in both files', () => {
+    expect(flatObj).toMatchObject({ '  host': 'hexlet.io' });
   });
 });
