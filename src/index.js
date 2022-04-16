a4bc746f78e7bc0d223c74f1462cdd5da8188c0f
morsa <voxfoot@gmail.com>
2022-04-16 16:17:41 +0500

    chore: change return

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import parsers from './parsers.js';
 import reduceDifference from './reduceDifference.js';
+import stylish from './stylish.js';
 
 export const stringify = (obj) => JSON.stringify(obj, null, 2).replaceAll('"', '').replaceAll(',', '');
 
@@ -8,7 +9,7 @@ const genDiff = (filepath1, filepath2) => {
   const data2 = parsers(filepath2);
 
   const diff = reduceDifference(data1, data2);
-  return stringify(diff);
+  return stylish(diff);
 };
 
 export default genDiff;
