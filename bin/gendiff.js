a52f7b71a88dd3ce3e514b8f0c8e0559ea34fc89
morsa <voxfoot@gmail.com>
2022-04-16 17:39:11 +0500

    style: add default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import genDiff from '../src/index.js';
+import stylish from '../src/stylish.js';
 
 const program = new Command();
 
@@ -9,9 +10,16 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    const formatter = program.opts().format;
+    switch (formatter) {
+      case ' here will other formatter':
+        console.log();
+        break;
+      default:
+        console.log(stylish(genDiff(filepath1, filepath2)));
+    }
   });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import parsers from './parsers.js';
 import reduceDifference from './reduceDifference.js';
-import stylish from './stylish.js';
-
-export const stringify = (obj) => JSON.stringify(obj, null, 2).replaceAll('"', '').replaceAll(',', '');
 
 const genDiff = (filepath1, filepath2) => {
   const data1 = parsers(filepath1);
   const data2 = parsers(filepath2);
 
   const diff = reduceDifference(data1, data2);
-  return stylish(diff);
+  return diff;
 };
 
 export default genDiff;
