7e0bac9901c8eed8f84307c8a087e8fef7134ffe
morsa <voxfoot@gmail.com>
2022-04-19 23:30:24 +0500

    refactor: reduceDifference function

--- src/reduceDifference.js
+++ src/reduceDifference.js
@@ -8,23 +8,21 @@ const reduceDifference = (data1, data2) => {
   const sortedUnionKeys = _.sortBy(unionKeys);
 
   const result = sortedUnionKeys
-    .map((key) => {
+    .reduce((acc, key) => {
       if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-        return { name: key, type: 'node', children: reduceDifference(data1[key], data2[key]) };
+        return { ...acc, [`${key}`]: { type: 'nested', value: reduceDifference(data1[key], data2[key]) } };
       }
       if (!_.has(data1, key)) {
-        return { name: key, type: 'plus', value: data2[key] };
+        return { ...acc, [`${key}`]: { type: 'added', value: data2[key] } };
       }
       if (!_.has(data2, key)) {
-        return { name: key, type: 'minus', value: data1[key] };
+        return { ...acc, [`${key}`]: { type: 'deleted', value: data1[key] } };
       }
       if (data1[key] !== data2[key]) {
-        return {
-          name: key, type: 'changed', firstValue: data1[key], secondValue: data2[key],
-        };
+        return { ...acc, [`${key}`]: { type: 'changed', previusValue: data1[key], currentValue: data2[key] } };
       }
-      return { name: key, type: 'unchanged', value: data1[key] };
-    });
+      return { ...acc, [`${key}`]: { type: 'unchanged', value: data1[key] } };
+    }, {});
 
   return result;
 };
