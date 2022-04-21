d01fc8334085eda05f630a3183338efc421d5ef4
morsa <voxfoot@gmail.com>
2022-04-21 08:03:58 +0500

    tests: refactor test stylish

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,59 +1,19 @@
 import { fileURLToPath } from 'url';
 import * as path from 'path';
-import parsers from '../src/parsers.js';
-import reduceDifference from '../src/reduceDifference.js';
+import fs from 'fs';
+import genDiff from '../src/index.js';
+import stylish from '../src/stylish.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const getPathJSONfile = (filename) => path.join(__dirname, '..', '__fixtures__', 'json', filename);
-const getPathYAMLfile = (filename) => path.join(__dirname, '..', '__fixtures__', 'yaml', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getExpectedPath = (filename) => path.join(__dirname, filename);
+const readFile = (filename) => fs.readFileSync(getExpectedPath(filename), 'utf-8');
 
 describe('genDiff', () => {
-  describe('flat', () => {
-    const verifyFlat = parsers(path.join(__dirname, 'verificationFlat.json'));
-
-    test('json', () => {
-      const json1 = parsers(getPathJSONfile('filepath1.json'));
-      const json2 = parsers(getPathJSONfile('filepath2.json'));
-      const flat = reduceDifference(json1, json2);
-
-      expect(flat).toStrictEqual(verifyFlat);
-    });
-
-    test('yaml', () => {
-      const yml1 = parsers(getPathYAMLfile('filepath1.yml'));
-      const yml2 = parsers(getPathYAMLfile('filepath2.yml'));
-      const flat = reduceDifference(yml1, yml2);
-
-      expect(flat).toStrictEqual(verifyFlat);
-    });
-
-    test('json & yml', () => {
-      const json = parsers(getPathJSONfile('filepath1.json'));
-      const yml = parsers(getPathYAMLfile('filepath2.yml'));
-      const flat = reduceDifference(json, yml);
-
-      expect(flat).toStrictEqual(verifyFlat);
-    });
-  });
-
-  describe('tree', () => {
-    const verifyTree = parsers(path.join(__dirname, 'verificationTree.json'));
-
-    test('json', () => {
-      const tree1 = parsers(getPathJSONfile('tree1.json'));
-      const tree2 = parsers(getPathJSONfile('tree2.json'));
-      const diffTree = reduceDifference(tree1, tree2);
-
-      expect(diffTree).toStrictEqual(verifyTree);
-    });
-
-    test('yml', () => {
-      const tree1 = parsers(getPathYAMLfile('tree1.yaml'));
-      const tree2 = parsers(getPathYAMLfile('tree2.yml'));
-      const diffTree = reduceDifference(tree1, tree2);
-
-      expect(diffTree).toStrictEqual(verifyTree);
-    });
+  test('stylish formatter', () => {
+    const tree1 = getFixturePath('tree1.json');
+    const tree2 = getFixturePath('tree2.json');
+    expect(stylish(genDiff(tree1, tree2))).toStrictEqual(readFile('expectStylish.txt'));
   });
 });
