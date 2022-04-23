f46d68be8721395f22a921f8a6bcef254d26883c
morsa <voxfoot@gmail.com>
2022-04-23 11:29:38 +0500

    refactor: plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,38 +1,40 @@
 import _ from 'lodash';
-import { fileURLToPath } from 'url';
-import * as path from 'path';
-import fs from 'fs';
-import genDiff from '../index.js';
+
+const getValue = (value) => (typeof value !== 'string' ? value : `'${value}'`);
 
 const plain = (diff) => {
-  const iter = (currentValue) => {
+  const iter = (currentValue, parent) => {
     if (!_.isObject(currentValue)) {
       return `${currentValue}`;
     }
 
     const lines = Object.entries(currentValue).map(([key, fild]) => {
       switch (fild.type) {
-        case 'nested':
-          return `'${key}.${iter(fild.value)}`;
         case 'added':
           return _.isObject(fild.value)
-            ? `${key}' was added with value: [complex value]`
-            : `${key}' was added with value: ${fild.value}`;
+            ? `Property '${parent}${key}' was added with value: [complex value]`
+            : `Property '${parent}${key}' was added with value: ${getValue(fild.value)}`;
         case 'deleted':
-          return `${key}' was removed`;
+          return `Property '${parent}${key}' was removed`;
+        case 'changed':
+          if (_.isObject(fild.previusValue)) {
+            return `Property '${parent}${key}' was updated. From [complex value] to ${getValue(fild.currentValue)}`;
+          }
+          if (_.isObject(fild.currentValue)) {
+            return `Property '${parent}${key}' was updated. From ${getValue(fild.previusValue)} to [complex value]`;
+          }
+          return `Property '${parent}${key}' was updated. From ${getValue(fild.previusValue)} to ${getValue(fild.currentValue)}`;
+        case 'unchanged':
+          return '';
+        default:
+          return iter(fild.value, `${parent}${key}.`);
       }
     });
 
-    return [...lines].join('\n');
+    return _.compact([...lines]).join('\n');
   };
 
   return iter(diff, '');
 };
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const getFixturePath = (filename) => path.join(__dirname, '../..', '__fixtures__', filename);
-
-console.dir(genDiff(getFixturePath('tree1.json'), getFixturePath('tree2.json')), { depth: null });
-console.log('-----');
-console.log(plain(genDiff(getFixturePath('tree1.json'), getFixturePath('tree2.json'))));
+export default plain;
