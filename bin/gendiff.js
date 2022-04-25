2c8e5e2384cdf7a0e2ee15e6cf4484d829f6b595
morsa <voxfoot@gmail.com>
2022-04-25 22:25:16 +0500

    refactor: bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,30 +1,16 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import genDiff from '../src/index.js';
-import stylish from '../src/formatters/stylish.js';
-import plain from '../src/formatters/plain.js';
-import json from '../src/formatters/json.js';
 
 const program = new Command();
 
 program
   .version('1.0.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    const formatter = program.opts().format;
-    switch (formatter) {
-      case 'plain':
-        console.log(plain(genDiff(filepath1, filepath2)));
-        break;
-      case 'json':
-        console.log(json(genDiff(filepath1, filepath2)));
-        break;
-      default:
-        console.log(stylish(genDiff(filepath1, filepath2)));
-    }
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
