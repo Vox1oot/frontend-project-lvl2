67edeb134972ab5b9deb3a89c513f5f94937b47f
morsa <voxfoot@gmail.com>
2022-04-25 22:22:23 +0500

    feature: implement func showFormat

---
+++ src/formatters/showFormat.js
@@ -0,0 +1,11 @@
+import stylish from './stylish.js';
+import plain from './plain.js';
+import json from './json.js';
+
+export default (diff, format) => {
+  switch (format) {
+    case 'plain': return plain(diff);
+    case 'json': return json(diff);
+    default: return stylish(diff);
+  }
+};
