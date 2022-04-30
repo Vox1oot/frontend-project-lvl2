98f64ebf6ca0f7eb1597bb63d1203084fa2ea0de
morsa <voxfoot@gmail.com>
2022-04-30 17:11:07 +0500

    test: refactor genDiff test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,10 @@
 import { fileURLToPath } from 'url';
 import * as path from 'path';
-import fs from 'fs';
 import genDiff from '../src/index.js';
+import readFile from '../src/readFile.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('genDiff', () => {
   const json1 = getFixturePath('tree1.json');
@@ -15,24 +12,12 @@ describe('genDiff', () => {
   const yaml1 = getFixturePath('tree1.yaml');
   const yaml2 = getFixturePath('tree2.yml');
 
-  const stylishJSON = genDiff(json1, json2);
-  const stylishYAML = genDiff(yaml1, yaml2);
-  const stylishExpect = readFile('expectStylish.txt');
-
-  const plainJSON = genDiff(json1, json2, 'plain');
-  const plainYAML = genDiff(yaml1, yaml2, 'plain');
-  const plainExpect = readFile('expectPlain.txt');
-
-  const structureJSON = genDiff(json1, json2, 'json');
-  const structureYAML = genDiff(yaml1, yaml2, 'json');
-  const jsonExpect = readFile('expectJSON.txt');
-
   test.each([
-    [stylishJSON, stylishYAML, stylishExpect],
-    [plainJSON, plainYAML, plainExpect],
-    [structureJSON, structureYAML, jsonExpect],
-  ])('formatters', (a, b, expected) => {
-    expect(a).toStrictEqual(expected);
-    expect(b).toStrictEqual(expected);
+    [genDiff(json1, json2), genDiff(yaml1, yaml2), readFile(getFixturePath('expectStylish.txt'))],
+    [genDiff(json1, json2, 'plain'), genDiff(yaml1, yaml2, 'plain'), readFile(getFixturePath('expectPlain.txt'))],
+    [genDiff(json1, json2, 'json'), genDiff(yaml1, yaml2, 'json'), readFile(getFixturePath('expectJSON.txt'))],
+  ])('formatters', (j, y, expected) => {
+    expect(j).toStrictEqual(expected);
+    expect(y).toStrictEqual(expected);
   });
 });
